0d78f36990eb9702d269e8efc6748a90db9f11f7
ddpms <qtddpms@gmail.com>
2023-01-03 21:54:24 +0900

    image classification trainer

---
+++ trainer/utils.py
@@ -0,0 +1,40 @@
+import os
+import torch
+import random
+import numpy as np
+
+
+def set_randomseed(random_seed: int):
+    os.environ["PYTHONHASHSEED"] = str(random_seed)
+    random.seed(random_seed)
+    np.random.seed(random_seed)
+    torch.manual_seed(random_seed)
+    torch.cuda.manual_seed(random_seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+
+class AverageMeter:
+    """ Computes and stores the average and current value
+    """
+
+    def __init__(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __repr__(self) -> str:
+        return f'AverageMeter(val={self.val}, avg={self.avg}, sum={self.sum}, count={self.count})'
